2e86c932a69e2ec60240c0db9be4fc8354b8c08e
nurdaulettn <nurdaulettolegen26@gmail.com>
2025-05-08 17:51:54 +0500

    adding pipeline for social auth that create profile after creating user

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -102,10 +102,22 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
 ]
 
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
+
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('googleKey')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('googleSecret')
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI='https://mysite.com:8000/social-auth/complete/google-oauth2/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
